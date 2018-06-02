2c4a375ddd5f2359d8d1021354151c5a03024548
Jamie Morales <18276704+jamiemorales@users.noreply.github.com>
2018-06-02 15:14:12 -0500

    Add files via upload

---
+++ 05-importing-data-in-python-1/01-introduction-and-flat-files/importing-text-files-line-by-line.py
@@ -0,0 +1,9 @@
+'''
+Open moby_dick.txt using the with context manager and the variable file. Print the first three lines of the file to the shell by using readline() three times within the context manager.
+'''
+
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
