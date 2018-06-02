813651e5cae18fc6a33a975df1147392ad072159
Jamie Morales <18276704+jamiemorales@users.noreply.github.com>
2018-06-02 15:36:18 -0500

    Add files via upload

---
+++ 05-importing-data-in-python-1/03-working-with-relational-databases-in-python/creating-a-database-engine.py
@@ -0,0 +1,9 @@
+'''
+Import the function create_engine from the module sqlalchemy. Create an engine to connect to the SQLite database 'Chinook.sqlite' and assign it to engine.
+'''
+
+# Import necessary module
+from sqlalchemy import create_engine
+
+# Create engine: engine
+engine = create_engine('sqlite:///Chinook.sqlite')
---
+++ 05-importing-data-in-python-1/03-working-with-relational-databases-in-python/the-power-of-sql-lies-in-relationship-between-tables-join.py
@@ -0,0 +1,13 @@
+'''
+Assign to rs the results from the following query: select all the records, extracting the Title of the record and Name of the artist of each record from the Album table and the Artist table, respectively. To do so, INNER JOIN these two tables on the ArtistID column of both. In a call to pd.DataFrame(), apply the method fetchall() to rs in order to fetch all records in rs. Store them in the DataFrame df. Set the DataFrame's column names to the corresponding names of the table columns.
+'''
+
+# Open engine in context manager
+# Perform query and save results to DataFrame: df
+with engine.connect() as con:
+    rs = con.execute("SELECT Title, Name from Album INNER JOIN Artist ON Artist.ArtistID = Album.ArtistID")
+    df = pd.DataFrame(rs.fetchall())
+    df.columns = rs.keys()
+
+# Print head of DataFrame df
+print(df.head())
